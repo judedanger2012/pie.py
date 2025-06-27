300a6789fdcf20198ffa6991c559bea3c64af89d
judedanger2012 <judeprivett2012@gmail.com>
2025-06-27 16:25:50 -0600

    Update pi.py
    added the done file for the pie game hope you enjoy.

--- pi.py
+++ pi.py
@@ -4,6 +4,7 @@ print ("in this game your trying to use your math skills to...")
 print ("help the baker finish his pies.")
 print ("each time you finish a math question you add a new diget to pi.")
 print ("but if you mess a question upp the baker will question your help.")
+print ("every time the baker questions your help he turns all succses to fails be warend.")
 print ("so have fun bake pies and dont mess up!")
 #intro to the game stateing some important themes.
 x = input("to start TYPE 1 ")
@@ -18,6 +19,11 @@ z = "3.141"
 b = "3"
 ty = "3.14159"
 bt = "3.141592"
+tt = "3.1415926"
+cc = "3.14159265"
+vv = "3.141592653"
+kk = "3.1415926535"
+fv = "3.14159265358"
 #variable list for the program.
 #start of questions and comments.
 if y == "3.1":
@@ -77,3 +83,43 @@ if x == "6m+4":
 else:
  print ("why you not read za recipe.")   
  print ("fail count 6")
+ if tt : ("3.1415926")
+print ("0.3y + 5 = 1 y =")
+x = input ("ANSEWR")    
+if x == "13.3":
+    print ("3.14159265")
+else:
+ print ("you be za worst baker me met scowls the baker.")   
+ print ("fail count 7")
+ if cc : ("3.14159265")
+print ("1x+5/7=9.X=")
+x = input ("ANSEWR")    
+if x == "58":
+    print ("3.141592653")
+else:
+ print ("i cant beleve you call yourself a baker.")   
+ print ("fail count 8")
+ if vv : ("3.141592653")
+print ("79z+9/-7=357.z=")
+x = input ("ANSEWR")    
+if x == "31.75":
+    print ("3.1415926535")
+else:
+ print ("why you not read za recipe.")   
+ print ("fail count 9")
+ if kk : ("3.1415926535")
+print ("you have come far my pupil but know it is time for our bake battle mawhahaha.the baker boss")
+print ("5(2x-3)=3(x+7)+4.x=")
+x = input ("ANSEWR")    
+if x == "40/7":
+    print ("3.14159265358")
+    print ("ah what a fine baker za pupel has become. ")
+    print ("i beleve you are ready know to run my backery.")
+    print ("you are chosen to take my place as...")
+    print ("i am retairing")
+    print ("THE END thnks so much for playing pie the game it means a lot as this is my first python program hope you enjoyed it frome the dev.")
+else:
+ print ("why you could not best me in our doughy dual you are to become my newst meat pie the baker. game over.")   
+ print ("fail count 10")
+ print ("try again to get the ending.")
+ 
