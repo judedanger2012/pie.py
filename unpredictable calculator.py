01d3aef5b7a1955d6d1e148d41c126dde0842ca5
judedanger2012 <judeprivett2012@gmail.com>
2025-07-23 15:52:17 -0600

    unpredictable calculator.py
    this calculator well is unexpected....

---
+++ unpredictable calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bi#!/usr/bin/env python3n/env python3
+while True:
+    print ("hello and welcome to your new favorite calculator.")
+    x = input ("your oporation")
+    input ("your oporation")
+    print ("print ok got it were doing") 
+    print (x)
+    y = input ("adative 1 ")
+    z = input ("adative 2  ")
+    print ("ok were doing") 
+    print (y) 
+    print (x)
+    print (z)
+    import random
+    my_list = ["banana", "7" , "how" "would i know" , "you" , "miku" , "i dont know" , "war" , "consult fish" , "a duck with a mohawk" , "bread" , "peace" , "why" , "a b c d e f g h i j k l m n o p q r s t u v w x y z" , "unknown"]
+    random.shuffle(my_list)
+    print ("the the ansewr after calculateing is is")
+    print (my_list[0])
+    print ("oh oh im terribly sorry but this result cant be right try once more.")
+    restart = input("do you want to retry")
+    if restart.lower() != "yes": 
+        break 
