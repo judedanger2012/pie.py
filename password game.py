92cb7f2e77baf6dfe9016d04a94a22f6b436471c
judedanger2012 <judeprivett2012@gmail.com>
2025-07-24 09:39:09 -0600

    a complex password puzzle game.py

---
+++ password game.py
@@ -0,0 +1,46 @@
+#!/usr/bi#!/usr/bin/env python3n/env python3
+print ("hi and welcome to your worst nightmare you frogot your password and well your password maiger is not happy so dont expect there help")
+my_list = ["5mwerss6758" , "mrw8esst7" "m7password8"]
+import random
+random.shuffle(my_list)
+x = (my_list[0])
+print ("ok ok so heres how this works i have 3 passwords you know none but you need to figure it out.")
+print (" i give hits your firsts one is all the charecters in the passwords 55mmmwwweessssss888rrr7776")
+print ("the next thing im going to say is a little somthing about the passwords")
+print ("one looks like a cat dancing on a key board the next is done by a narsisist teacher and the third is simple and bad for serurity.")
+input ("password")
+if input == (x):
+    print ("wow ok do we have a hacker or a cheater you dont dont just guess luckey.")
+else:
+    print ("oh well good try guess you cant beat this computer")
+    print ("here try again im not cracking a hint yet")
+    if input == (x):
+        print ("no no no you cant you just failed no cheating!!!!")
+    else:
+        print ("fine here since you the user is aparently stupid it starts with etheir 5 or a m. got it")
+        input ("password")
+        if input == (x):
+            print ("ok user maybe luck BUT I DONT THINK SO DONT CHEAT!!!!")
+        else:
+            print ("wow youve really froggoten again")
+            input ("password")
+            if input == (x):
+                print ("YOU JUST MESSED UP YOU CANT JUST MATERILIZE YOUR PASSWORD CHEATER!!!!")
+                
+                print ("OK YEAH YOU REALLY SUCK LOOK JUST JUST TAKE IT OK")
+                print(x)
+                print ("THERE SEE ITS SIMPLE REMEMBER NOW DUMB DUMB")
+                Y = ("YES")
+                print ("YOUR THOUGHTS : YOU KNOW THIS IS A VERY DISRESPECTFULL COMPUTER IS IT TIME TO SMASH IT.")
+                input ("yes if you want to destroy it")
+                if input == Y:
+                    input ("what object is used to destroy your stupid snarky computer.")
+                    w =input ("what object is used to destroy your stupid snarky computer.")
+                    print ("user violently destroyed computer with")(w)
+                    print ("we have finally put an end to this madness")
+                else:
+                    print ("no no destruction is not the ansewr but then what is")
+                    input ("your new solution")
+                    s = input ("your new solution.")
+                    print (s)("been used to effectivelly sort out your computers iratation")
+                    print ("the end")
