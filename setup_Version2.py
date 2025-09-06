d2b75b71f390f5958186a91ba244349fd6df2fd9
judedanger2012 <judeprivett2012@gmail.com>
2025-09-06 11:45:17 -0600

    Add files via upload
    idk

---
+++ setup_Version2.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="piepy",
+    version="0.1.0",
+    packages=["piepy"],
+    install_requires=[],
+    author="judedanger2012",
+    description="pie.py is a math based game where you do problems to collect digits of pi for a baker.",
+    python_requires=">=3.6",
+)
